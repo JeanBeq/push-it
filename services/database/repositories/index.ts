d51c94b0aa9c2e1d92e0c03d0515d36cf5d13627
JeanBeq <jean.bequilleux@gmail.com>
2025-10-08 16:23:53 +0200

    chore: centralize repository exports

---
+++ services/database/repositories/index.ts
@@ -0,0 +1,5 @@
+// Export de tous les repositories pour un accès facile
+export { exerciseRepository } from './exercise.repository';
+export { programRepository } from './program.repository';
+export { sessionRepository } from './session.repository';
+
